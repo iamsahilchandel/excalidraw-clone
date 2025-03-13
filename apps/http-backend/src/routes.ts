36aa3fdd8bbda1cfc81da5046badebeedf962e3e
Sahil Chandel <rajputsahilchandel@gmail.com>
2025-03-13 06:28:35 +0530

    Feat: used common types

--- apps/http-backend/src/routes.ts
+++ apps/http-backend/src/routes.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, Router } from "express";
 import { middleware } from "./middleware";
 import { JWT_SECRET } from "./config";
 import jwt from "jsonwebtoken";
+import { UserSchema } from "@repo/common/types";
 
 const router: Router = express.Router();
 
@@ -13,10 +14,19 @@ router.post("/login", (req: Request, res: Response) => {
   res.json({ token });
 });
 
-router.post("/register", (req: Request, res: Response) => {});
+router.post("/register", (req: Request, res: Response) => {
+  const { success, data, error } = UserSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  const { name, email, password } = data;
+
+  res.json({ message: "User registered successfully" });
+});
 
 router.post("/room", middleware, (req: Request, res: Response) => {
-    res.json({ roomId: "1234" });
+  res.json({ roomId: "1234" });
 });
 
 export default router;
