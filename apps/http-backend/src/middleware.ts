cf26206e397cf9741d5fdcc45904b78345f1c79c
Sahil Chandel <rajputsahilchandel@gmail.com>
2025-03-15 03:27:10 +0530

    Fix: updated decode issue

--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -2,8 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import { JWT_SECRET } from "./config";
 import jwt from "jsonwebtoken";
 
+export interface CustomRequest extends Request {
+  userId: string;
+}
+
 export function middleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.replace("Bearer ", "");
 
   if (!token) {
     console.info("Unauthorized Request: User token not found.");
@@ -13,9 +17,9 @@ export function middleware(req: Request, res: Response, next: NextFunction) {
 
   const decoded = jwt.verify(token, JWT_SECRET!);
 
-  if (decoded) {
-    // @ts-ignore - TODO: add types for decoded.userId
-    req.body.userId = decoded.userId;
+  if (decoded && typeof decoded === "object" && "userId" in decoded) {
+    // @ts-ignore 
+    req.userId = decoded.userId;
   } else {
     console.info("Unauthorized Request: Invalid token.");
     res.status(403).json({ message: "Unauthorized" });
